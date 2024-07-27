e766489a9a4aa00f665db2c7805edb197b338f7d
EmanuelSouzaMartins <emanuel.martins@aluno.ifsp.edu.br>
2024-07-27 18:18:54 -0300

    Cadastro de usuario arrumado!

---
+++ resources/views/user/__form.blade.php
@@ -0,0 +1,42 @@
+<section class="login">
+
+      <div class="wrapper">
+        <img src="../img/logo.png" class="login__logo">
+
+        <h1 class="login__title">Fazer cadastro</h1>
+    
+        <label class="login__label">
+          <span>nome de usuário</span>
+          <input type="text" name="name" id="name" class="input">
+        </label>
+
+        <label class="login__label">
+          <span>email</span>
+          <input type="text" name="email" id="email" class="input">
+        </label>
+  
+        <label class="login__label">
+          <span>senha</span>
+          <input type="password" name="password" id="password" class="input">
+        </label>
+  
+        <div class="login__icons">
+          <button type="button" class="icons__button">
+            <img src="../img/facebook-icon.png" alt="facebok">
+          </button>
+  
+          <button type="button" class="icons__button">
+            <img src="../img/google-icon.png" alt="facebok">
+          </button>
+  
+          <button type="button" class="icons__button">
+            <img src="../img/apple-icon.png" alt="facebok">
+          </button>
+        </div>
+  
+        <label class="login__label--checkbox">
+          <input type="checkbox" class="input--checkbox">
+          Manter login
+        </label>
+      </div>
+</section>
