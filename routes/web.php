2df9081a64e35dd7dc58af5c243f1b06349be8f3
brunovalenz <brunovalenz@gmail.com>
2024-08-05 08:28:02 -0300

    home?

--- routes/web.php
+++ routes/web.php
@@ -116,3 +116,8 @@ Route::prefix('user') -> group(function(){
     Route::put('/update/{id}', [UserController::class, 'update'])->name('user.update');#o id é chave primeira da tabela para identifcar quem estou modificando
     Route::delete('/destroy/{id}', [UserController::class, 'destroy'])->name('user.destroy');#estou passando parametro para o servidor
 });
+
+Route::prefix('home') -> group(function(){
+
+    Route::any('/jogar' , [HomeController::class,'jogar'])->name('home.jogar');    
+});
